4de3eeaf04a288d4f2b2769fe586356a6ad802e7
Thịnh <truongthinhm1@gmail.com>
2025-03-14 14:49:18 +0700

    modified:   addons/Custom_module/models/maintenance_request.py

--- addons/Custom_module/models/maintenance_request.py
+++ addons/Custom_module/models/maintenance_request.py
@@ -2,7 +2,10 @@ from odoo import models, fields, api
 
 class MaintenanceRequest(models.Model):
     _name = "maintenance.request"
-    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _description = "Maintenance Request"
+
+    name = fields.Char(string="Request Name", required=True)
+    category_id = fields.Many2one('maintenance.category', string="Category", ondelete="cascade")
     
     state = fields.Selection([
         ('draft', 'Draft'),
