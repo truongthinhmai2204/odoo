dfdcbcd0991c69af67ee0640c94475087720a4b4
Thịnh <truongthinhm1@gmail.com>
2025-03-14 14:48:02 +0700

    modified:   addons/Custom_module/models/__init__.py 	new file:   addons/Custom_module/models/maintenance_category.py

---
+++ addons/Custom_module/models/maintenance_category.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class MaintenanceCategory(models.Model):
+    _name = "maintenance.category"
+    _description = "Maintenance Category"
+
+    name = fields.Char(string="Category Name", required=True)
+    request_ids = fields.One2many('maintenance.request', 'category_id', string="Requests")
