6277df7aa4d859a336c57826d8e1c73e8e5d39af
Thịnh <truongthinhm1@gmail.com>
2025-03-14 15:06:28 +0700

    modified:   addons/Custom_module/models/maintenance_request.py

--- addons/Custom_module/models/maintenance_request.py
+++ addons/Custom_module/models/maintenance_request.py
@@ -9,6 +9,7 @@ class MaintenanceRequest(models.Model):
     category_id = fields.Many2one('maintenance.category', string="Category", ondelete="cascade")
     maintenance_team_id = fields.Many2one('maintenance.team', string="Maintenance Team", ondelete="set null")
     request_date = fields.Date(string="Request Date", default=fields.Date.today)
+    close_date = fields.Date(string="Close Date")
     stage_id = fields.Many2one(
         'maintenance.stage', 
         string="Stage", 
