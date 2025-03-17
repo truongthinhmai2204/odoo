d34beefd5e50b715a7712ff870c65881d97497bf
Thịnh <truongthinhm1@gmail.com>
2025-03-17 11:16:34 +0700

    modified:   addons/Custom_module/models/maintenance_category.py 	modified:   addons/Custom_module/models/maintenance_request.py

--- addons/Custom_module/models/maintenance_category.py
+++ addons/Custom_module/models/maintenance_category.py
@@ -6,3 +6,8 @@ class MaintenanceCategory(models.Model):
 
     name = fields.Char(string="Category Name", required=True)
     request_ids = fields.One2many('maintenance.request', 'category_id', string="Requests")
+
+    def unlink(self):
+        for category in self:
+            category.request_ids.unlink()
+        return super(MaintenanceCategory, self).unlink()
