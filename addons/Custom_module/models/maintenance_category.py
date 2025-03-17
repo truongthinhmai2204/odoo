d2e05020b2ca7398c6a5333ffca2b8529cf10ce3
Thịnh <truongthinhm1@gmail.com>
2025-03-17 15:45:30 +0700

    modified:   addons/Custom_module/models/maintenance_category.py

--- addons/Custom_module/models/maintenance_category.py
+++ addons/Custom_module/models/maintenance_category.py
@@ -2,11 +2,9 @@ from odoo import models, fields
 
 class MaintenanceCategory(models.Model):
     _name = "maintenance.category"
+    _description = "Maintenance Category"
 
-    name = fields.Char(string="Category Name", required=True)
-    request_ids = fields.One2many('maintenance.request', 'category_id', string="Requests")
-
-    def unlink(self):
-        for category in self:
-            category.request_ids.unlink()
-        return super(MaintenanceCategory, self).unlink()
+    request_ids = fields.One2many(
+        'maintenance.request', 'category_id',
+        string="Requests"
+    )
