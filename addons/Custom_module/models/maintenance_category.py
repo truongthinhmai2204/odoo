8c5ede481bfc64bdb44950cc685efbdf792ce29d
Thịnh <truongthinhm1@gmail.com>
2025-03-18 09:18:16 +0700

    modified:   addons/Custom_module/models/maintenance_category.py

--- addons/Custom_module/models/maintenance_category.py
+++ addons/Custom_module/models/maintenance_category.py
@@ -4,7 +4,9 @@ class MaintenanceCategory(models.Model):
     _name = "maintenance.category"
     _description = "Maintenance Category"
 
+    name = fields.Char(string="Category Name", required=True)
     request_ids = fields.One2many(
-        'maintenance.request', 'category_id',
+        'maintenance.request',  
+        'category_id',  
         string="Requests"
     )
