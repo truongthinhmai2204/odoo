4365bea497108b16a987e192d025f84e8b3d4efe
Thịnh <truongthinhm1@gmail.com>
2025-03-20 14:26:17 +0700

    modified:   addons/Custom_module/models/maintenance_request.py

--- addons/Custom_module/models/maintenance_request.py
+++ addons/Custom_module/models/maintenance_request.py
@@ -16,7 +16,7 @@ class MaintenanceRequest(models.Model):
         ondelete="set null"
     )
 
-    owner_user_id = fields.Many2one('res.users', string="Owner User", related="some_field.owner_user_id", store=True)
+    owner_user_id = fields.Many2one('res.users', string="Owner User", store=True)
 
     category_id = fields.Many2one(
         'maintenance.category', 
