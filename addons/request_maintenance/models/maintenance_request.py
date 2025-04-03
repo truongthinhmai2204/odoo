d9ccd65039e88a21c16db4b0ee2d100e4818bb06
Thịnh <truongthinhm1@gmail.com>
2025-04-03 15:47:50 +0700

    modified:   addons/request_maintenance/models/maintenance_request.py

--- addons/request_maintenance/models/maintenance_request.py
+++ addons/request_maintenance/models/maintenance_request.py
@@ -28,6 +28,7 @@ class MaintenanceRequest(models.Model):
     device_ids = fields.One2many('stock.move', 'order_id')
     user_id = fields.Many2one('res.users', string='Owner', tracking=True)
     owner_user_id = fields.Many2one('res.users', string='Created by User', default=lambda s: s.env.uid)
+    employee_id = fields.Many2one('hr.employee', string="Employee")
 
     stock_status = fields.Selection([
         ('available', 'Available in Stock'),
