34db71c0e57e813371e2f4d08b1beda213910cd9
Thịnh <truongthinhm1@gmail.com>
2025-04-04 08:54:24 +0700

    modified:   addons/request_maintenance/models/maintenance_category.py 	modified:   addons/request_maintenance/models/maintenance_equipment.py 	modified:   addons/request_maintenance/models/maintenance_stage.py 	modified:   addons/request_maintenance/models/maintenance_team.py

--- addons/request_maintenance/models/maintenance_equipment.py
+++ addons/request_maintenance/models/maintenance_equipment.py
@@ -8,6 +8,12 @@ class MaintenanceEquipment(models.Model):
     description = fields.Text(string="Description")
     user_id = fields.Many2one('res.users', string='Owner', tracking=True)
     owner_user_id = fields.Many2one('res.users', string='Created by User', default=lambda s: s.env.uid)
+    alias_id = fields.Many2one(
+        'mail.alias', 'Alias', ondelete='restrict', required=True,
+        help="Email alias for this equipment category. New emails will automatically "
+        "create a new equipment under this category.")
+    device_id = fields.Many2one('stock.picking', string="Thiết bị", inverse_name='order_id')
+    device_ids = fields.One2many('stock.move', 'order_id')
     employee_id = fields.Many2one('hr.employee', string="Employee")
     category_id = fields.Many2one('maintenance.category', string="Category", ondelete="cascade")
     order_id = fields.Many2one('sale.order', string="Order", ondelete="cascade", required=True)
