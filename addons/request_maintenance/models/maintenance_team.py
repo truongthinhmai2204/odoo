a1b3f143ff4e122231e794430b8282a64ccadde8
Thịnh <truongthinhm1@gmail.com>
2025-04-08 14:11:34 +0700

    modified:   addons/request_maintenance/models/maintenance_category.py 	modified:   addons/request_maintenance/models/maintenance_equipment.py 	modified:   addons/request_maintenance/models/maintenance_request.py 	modified:   addons/request_maintenance/models/maintenance_stage.py 	modified:   addons/request_maintenance/models/maintenance_team.py 	modified:   addons/request_maintenance/models/stock_move.py

--- addons/request_maintenance/models/maintenance_team.py
+++ addons/request_maintenance/models/maintenance_team.py
@@ -10,7 +10,7 @@ class MaintenanceTeam(models.Model):
     owner_user_id = fields.Many2one('res.users', string='Created by User', default=lambda s: s.env.uid)
     device_ids = fields.One2many('stock.move', 'maintenance_request_id', string="Devices")
     employee_id = fields.Many2one('hr.employee', string="Employee")
-    category_id = fields.Many2one('maintenance.category', string="Category", ondelete="set null")
+    category_id = fields.Many2one('maintenance.category', string="Category", ondelete="cascade")
     maintenance_request_id = fields.Many2one('maintenance.request', string="Maintenance Request")
     product_id = fields.Many2one('product.product', string='Related Product')
 
