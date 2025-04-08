3a65b8f72f74562697871f3e0e51b9660e3dcc8f
Thịnh <truongthinhm1@gmail.com>
2025-04-08 13:04:21 +0700

    modified:   addons/request_maintenance/models/__init__.py 	deleted:    addons/request_maintenance/models/stock_move.py

--- addons/request_maintenance/models/__init__.py
+++ addons/request_maintenance/models/__init__.py
@@ -3,4 +3,3 @@ from . import maintenance_category
 from . import maintenance_team
 from . import maintenance_equipment
 from . import maintenance_stage
-from . import stock_move
--- addons/request_maintenance/models/stock_move.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class StockMove(models.Model):
-    _inherit = 'stock.move'
-
-    maintenance_request_id = fields.Many2one('maintenance.request', string='Maintenance Request')
