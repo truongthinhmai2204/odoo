360d3a4c1427ed568ff699c4a610e8c78de2b149
Thịnh <truongthinhm1@gmail.com>
2025-04-10 14:34:25 +0700

    modified:   addons/request_maintenance/__manifest__.py

--- addons/request_maintenance/__manifest__.py
+++ addons/request_maintenance/__manifest__.py
@@ -8,8 +8,8 @@
     'website': 'https://yourcompany.com',
     'depends': ['mail'], 
     'data': [
-        'security/ir.model.access.csv',  
         'security/ir.rule.xml',         
+        'security/ir.model.access.csv',  
         'data/maintenance_request_data.xml',      
         'data/maintenance_stage.xml',
         'views/maintenance_request.xml', 
