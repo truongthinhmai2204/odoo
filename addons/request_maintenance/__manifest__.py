504f8e298ed621bd867b3ba0edf7ddbb6d73a63a
Thịnh <truongthinhm1@gmail.com>
2025-04-15 11:20:14 +0700

    modified:   addons/request_maintenance/__manifest__.py

--- addons/request_maintenance/__manifest__.py
+++ addons/request_maintenance/__manifest__.py
@@ -10,10 +10,10 @@
     'data': [
         'data/maintenance_request_data.xml',      
         'data/maintenance_stage.xml',
-        'views/maintenance_request.xml', 
-        'views/maintenance_category.xml',
         'security/ir.rule.xml',         
         'security/ir.model.access.csv',  
+        'views/maintenance_request.xml', 
+        'views/maintenance_category.xml',
     ],
     'application': True,
     'installable': True,
