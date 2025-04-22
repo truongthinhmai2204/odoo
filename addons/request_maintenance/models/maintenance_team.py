281e8caa02f03b0230095688fbeb1f41761486ba
Thịnh <truongthinhm1@gmail.com>
2025-04-22 14:46:37 +0700

    modified:   addons/request_maintenance/models/maintenance_team.py

--- addons/request_maintenance/models/maintenance_team.py
+++ addons/request_maintenance/models/maintenance_team.py
@@ -9,6 +9,10 @@ class MaintenanceTeam(models.Model):
     request_ids = fields.One2many('maintenance.request', 'maintenance_team_id', string="Requests")
     user_id = fields.Many2one('res.users', string='Technician', tracking=True)
     color = fields.Integer("Color Index", default=0)
+    active = fields.Boolean(string="Active", default=True)
+    member_ids = fields.Many2many(
+        'res.users', 'maintenance_team_users_rel', string="Team Members",
+        domain="[('company_ids', 'in', company_id)]")
     owner_user_id = fields.Many2one('res.users', string='Created by User', default=lambda s: s.env.uid)
     employee_id = fields.Many2one('hr.employee', string="Employee")
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, index=True)
