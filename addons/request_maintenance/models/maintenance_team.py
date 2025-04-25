0d6ae748f2b7ca87e500c949315434dc0c4cee7b
Thịnh <truongthinhm1@gmail.com>
2025-04-25 15:47:51 +0700

    modified:   addons/request_maintenance/models/maintenance_team.py

--- addons/request_maintenance/models/maintenance_team.py
+++ addons/request_maintenance/models/maintenance_team.py
@@ -17,17 +17,23 @@ class MaintenanceTeam(models.Model):
     employee_id = fields.Many2one('hr.employee', string="Employee")
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, index=True)
     request_ids = fields.One2many('maintenance.request', 'maintenance_team_id', copy=False)
-    todo_request_ids = fields.One2many(
-    'maintenance.request', 'maintenance_team_id',
-    string="To-do Requests"
-    )
+    todo_request_ids = fields.One2many('maintenance.request', string="Requests", copy=False, compute='_compute_todo_requests')
+    todo_request_count = fields.Integer(string="Number of Requests", compute='_compute_todo_requests')
+    todo_request_count_date = fields.Integer(string="Number of Requests Scheduled", compute='_compute_todo_requests')
+    todo_request_count_high_priority = fields.Integer(string="Number of Requests in High Priority", compute='_compute_todo_requests')
+    todo_request_count_block = fields.Integer(string="Number of Requests Blocked", compute='_compute_todo_requests')
+    todo_request_count_unscheduled = fields.Integer(string="Number of Requests Unscheduled", compute='_compute_todo_requests')
 
-    #dashboard
     @api.depends('request_ids.stage_id.done')
     def _compute_todo_requests(self):
         for team in self:
             team.todo_request_ids = self.env['maintenance.request'].search([('maintenance_team_id', '=', team.id), ('stage_id.done', '=', False)])
-           
+            team.todo_request_count = len(team.todo_request_ids)
+            team.todo_request_count_date = self.env['maintenance.request'].search_count([('maintenance_team_id', '=', team.id), ('schedule_date', '!=', False)])
+            team.todo_request_count_high_priority = self.env['maintenance.request'].search_count([('maintenance_team_id', '=', team.id), ('priority', '=', '3')])
+            team.todo_request_count_block = self.env['maintenance.request'].search_count([('maintenance_team_id', '=', team.id), ('kanban_state', '=', 'blocked')])
+            team.todo_request_count_unscheduled = self.env['maintenance.request'].search_count([('maintenance_team_id', '=', team.id), ('schedule_date', '=', False)])
+
     @api.depends('equipment_ids')
     def _compute_equipment(self):
         for team in self:
