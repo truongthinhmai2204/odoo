b8c54b11bca0cf287b0dacd37c17fb4c550ff152
Thịnh <truongthinhm1@gmail.com>
2025-04-26 13:27:09 +0700

    modified:   addons/request_maintenance/models/__init__.py

--- addons/request_maintenance/models/__init__.py
+++ addons/request_maintenance/models/__init__.py
@@ -1,6 +1,6 @@
 from . import maintenance_request_custom
 from . import maintenance_equipment_category_custom
 from . import maintenance_team_custom
-from . import maintenance_equipment_cus
+from . import maintenance_equipment_custom
 from . import maintenance_stage_custom
 from . import stock_move
