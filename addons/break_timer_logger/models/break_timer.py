dd3583f581407fbe865803f4f68819b13cad501f
Thịnh <truongthinhm1@gmail.com>
2025-05-09 10:34:28 +0700

    modified:   addons/break_timer_logger/models/break_timer.py

--- addons/break_timer_logger/models/break_timer.py
+++ addons/break_timer_logger/models/break_timer.py
@@ -10,7 +10,6 @@ class BreakSession(models.Model):
     duration = fields.Float(string="Duration (minutes)", compute="_compute_duration", store=True)
     report_id = fields.Many2one('break.report', string="Report")
 
-
     @api.depends('start_time', 'end_time')
     def _compute_duration(self):
         for rec in self:
@@ -28,6 +27,7 @@ class BreakConfig(models.Model):
     work_duration = fields.Integer(string="Work Duration")
     break_duration = fields.Integer(string="Break Duration")
     repeat_count = fields.Integer(string="Repeat Count")
+    report_id = fields.Many2one('break.report', string="Report")
 
 class BreakReport(models.Model):
     _name = 'break.report'
@@ -37,3 +37,4 @@ class BreakReport(models.Model):
     total_work = fields.Float(string="Total Work (min)")
     total_break = fields.Float(string="Total Break (min)")
     session_ids = fields.One2many('break.session', 'report_id', string="Sessions")
+    config_ids = fields.One2many('break.config', 'report_id', string="Configurations") 
