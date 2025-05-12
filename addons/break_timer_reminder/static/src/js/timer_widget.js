5aba974fbf3ba4bc4a763c257172546d84cc6e5a
Thịnh <truongthinhm1@gmail.com>
2025-05-12 11:13:39 +0700

    modified:   addons/break_timer_reminder/static/src/js/timer_widget.js

--- addons/break_timer_reminder/static/src/js/timer_widget.js
+++ addons/break_timer_reminder/static/src/js/timer_widget.js
@@ -43,6 +43,6 @@ class TimerWidget extends Component {
     }
 }
 
-TimerWidget.template = "break_timer_reminder.timer_widget";
+TimerWidget.template = "break_timer_reminder.TimerWidget";
 
 registry.category("actions").add("break_timer_reminder.timer_widget", TimerWidget);
