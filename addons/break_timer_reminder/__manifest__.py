12ce703b429fe707335fb842dd96966d8d42ad40
Thịnh <truongthinhm1@gmail.com>
2025-05-21 14:40:03 +0700

    modified:   addons/break_timer_reminder/__manifest__.py

--- addons/break_timer_reminder/__manifest__.py
+++ addons/break_timer_reminder/__manifest__.py
@@ -11,7 +11,6 @@
         "data/break_config.xml",
         "views/break_config_views.xml",
         "views/break_session_views.xml",
-        "views/assets.xml",
         "views/menu.xml",
     ],
     "assets": {
